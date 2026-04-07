ab32f70c4bdeabb2860bf4087b5e17ce3ae9f89a
moksh010101 <mokshgautam94041@gmail.com>
2026-04-08 01:10:50 +0530

    Add files via upload

---
+++ web.devlopement.SEM.2/script.js
@@ -0,0 +1,28 @@
+//console.log("hello world");
+//document.write("hello world");
+var num=10;
+console.log(num)
+console.log(typeof(num));
+num =false;
+console.log(num);
+console.log(typeof(num));
+num=null
+console.log(num);
+console.log(typeof(num));
+num=undefined
+console.log(num)  
+console.log(typeof(num));
+const synl =symbol[4];
+    console.log(sym1);
+const syn2=symbol[4];
+console.log(sym2)
+if(sym1==sym2){
+     console.log("true");
+}
+else{
+    console.log("false");
+}
+var a=100; b=13; var c=
+   
+
+
